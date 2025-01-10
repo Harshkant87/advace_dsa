7fff0016f997ed98548e8c2541b32654e25ce401
Harsh Kant <harshkant87@gmail.com>
2025-01-10 07:44:14 +0530

    Find target in a sorted array

---
+++ find_target.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Time complexity: O(logn)
+
+int binarySearch(vector<int> arr, int target){
+    int n = arr.size();
+    int left = 0;
+    int right = n - 1;
+    while(left <= right){
+        int mid = left + (right - left) / 2;
+        if(arr[mid] == target){
+            return mid;
+        }
+        else if(arr[mid] < target){
+            left = mid + 1;
+        }
+        else{
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int recursiveBinarySearch(vector<int> arr, int target, int left, int right){
+    if(left > right){
+        return -1;
+    }
+    int mid = left + (right - left) / 2;
+    if(arr[mid] == target){
+        return mid;
+    }
+    else if(arr[mid] < target){
+        return recursiveBinarySearch(arr, target, mid + 1, right);
+    }
+    else{
+        return recursiveBinarySearch(arr, target, left, mid - 1);
+    }
+}
+
+int main(){
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int target = 3;
+    int n = arr.size();
+    int index = binarySearch(arr, target);
+    if(index == -1){
+        cout<<"target: "<<target<<" not found"<<endl;
+    }
+    else
+        cout<<"target: "<<target<<" found at index: "<<index<<endl;
+
+    cout<<"Recursive binary search"<<endl;
+    int index2 = recursiveBinarySearch(arr, target, 0, n - 1);
+    if(index2 == -1){
+        cout<<"target: "<<target<<" not found"<<endl;
+    }
+    else
+        cout<<"target: "<<target<<" found at index: "<<index2<<endl;
+}
